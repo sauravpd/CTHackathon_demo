c975385170fd09d14a11c4805b001ed91c17b544
saurav <sauravpd58@gmail.com>
2019-03-30 11:18:22 +0530

    commit changes

--- src/main/java/pages/CTHackATAhon.java
+++ src/main/java/pages/CTHackATAhon.java
@@ -16,6 +16,22 @@ public class CTHackATAhon extends PageObject
 
 	@FindBy(xpath = "//*[contains(text(),'#CTHackATAhon')]")
 	private List<WebElement> CTHackATAhonText;
+	
+	@FindBy(xpath = "//input[@id ='keyword']")
+	private WebElement Searchbox;
+	
+	@FindBy(xpath = "//li[@class = 'selected']/a")
+	private WebElement company;
+	
+	@FindBy(xpath = "//span[@id = 'symbol']")
+	private WebElement symbol;
+	
+	@FindBy(xpath = "//ul[@class = 'stock']//div[@id = 'dayHigh']")
+	private WebElement high;
+	
+	@FindBy(xpath = "//ul[@class = 'stock']//div[@id = 'dayLow']")
+	private WebElement low;
+	
 
 	public CTHackATAhon(WebDriver driver) 
 	{
--- src/main/java/tests/CTHackATAhonTest.java
+++ src/main/java/tests/CTHackATAhonTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
@@ -12,11 +13,33 @@ public class CTHackATAhonTest extends BaseTest
 	public static Logger LOG=Logger.getLogger(CTHackATAhonTest.class);
 
 	@Test
-	public void loginTest()
+	public void cTHackATAhonCountTest()
 	{
-		LOG.info("****CTHackATAhon Test****");
+		LOG.info("***cTHackATAhonCountTest****");
 		CTHackATAhon cTHackATAhon = new CTHackATAhon(driver);
 		LOG.info("#CTHackATAhon count :"+cTHackATAhon.getCount());		
-
+		Assert.assertEquals(2, cTHackATAhon.getCount());
 	}	
+	
+	@Test
+	public void NSETopGainersTest()
+	{
+		LOG.info("****NSETopGainersTest****");
+		CTHackATAhon cTHackATAhon = new CTHackATAhon(driver);	
+	}	
+	
+	@Test
+	public void NSETopLosersTest()
+	{
+		LOG.info("****NSETopLosersTest****");
+		CTHackATAhon cTHackATAhon = new CTHackATAhon(driver);	
+	}	
+	
+	@Test
+	public void NSEQuotesTest()
+	{
+		LOG.info("****NSEQuotesTest****");
+		CTHackATAhon cTHackATAhon = new CTHackATAhon(driver);	
+	}	
+	
 }
